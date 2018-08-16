f295f765c402090f9056d64ad0406bc953d5634d
Torben J <torbenjepsen@outlook.com>
2018-08-16 17:37:14 -0500

    completed last challenge

--- challenges/challenge5.js
+++ challenges/challenge5.js
@@ -41,7 +41,18 @@ function appendDom(){
 
 function fixDom($el){
   // YOUR CODE HERE
-
+  $el.append('<p>The people upstairs all practise ballet</p>');
+  $el.append('<p>Their living room is a bowling alley</p>');
+  $el.append('<p>Their bedroom is full of conducted tours.</p>');
+  $el.append('<p>Their radio is louder than yours,</p>');
+  $el.append('<p>They celebrate week-ends all the week.</p>');
+  $el.append('<p>When they take a shower, your ceilings leak.</p>');
+  $el.append('<p>They try to get their parties to mix</p>');
+  $el.append('<p>By supplying their guests with Pogo sticks,</p>');
+  $el.append('<p>And when their fun at last abates,</p>');
+  $el.append('<p>They go to the bathroom on roller skates.</p>');
+  $el.append('<p>I might love the people upstairs more</p>');
+  $el.append('<p>If only they lived on another floor.</p>');
   // What is $el?
   console.log($el);
 }
